403820b5eaef8e393d62742d098ac45b933b814f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-05-09 17:07:45 +0300

    BasePlugin Implementation

--- bungee/src/main/java/net/streamline/api/base/BasePlugin.java
+++ bungee/src/main/java/net/streamline/api/base/BasePlugin.java
@@ -1,7 +1,25 @@
 package net.streamline.api.base;
 
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
+import java.util.logging.Logger;
+
 public abstract class BasePlugin extends Plugin {
-    
+        String name;
+        String version;
+        BasePlugin instance;
+
+        @Override
+        public void onEnable() {
+            name = "StreamlineAPI";
+            version = "${project.version}";
+        }
+
+        abstract public void enable();
+        abstract public void disable();
+        abstract public void reload();
+
+        public String getName() {return this.name;}
+        public String getVersion() {return this.version;}
 }
