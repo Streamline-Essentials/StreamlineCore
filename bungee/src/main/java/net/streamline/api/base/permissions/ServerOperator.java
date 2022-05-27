5b0ec2c7fa2fbf4399196b5376ef14bbc09eb76d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-05-27 03:59:32 +0300

    Create ServerOperator.java

---
+++ bungee/src/main/java/net/streamline/api/base/permissions/ServerOperator.java
@@ -0,0 +1,8 @@
+package net.streamline.api.base.permissions;
+
+/**
+ * Represents an object that may become a server operator, such as a {@link net.md_5.bungee.api.connection.ProxiedPlayer}
+ */
+public interface ServerOperator {
+
+}
